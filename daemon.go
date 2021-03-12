b93b5cc806891b96b2b8bc7e0bbbae9eb73b651b
Julien WITTOUCK <julien.wittouck@gmail.com>
2021-03-12 09:34:51 +0100

    ⚗️ : experimenting go-streamdeck lib

---
+++ daemon.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	streamdeck "github.com/magicmonkey/go-streamdeck"
+	_ "github.com/magicmonkey/go-streamdeck/devices"
+	"image/color"
+	"time"
+)
+
+func main() {
+	device, err := streamdeck.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	device.ClearButtons()
+
+	fmt.Printf("Found device %s\n", device.GetName())
+
+	// A simple yellow button in position 26
+	device.WriteColorToButton(0, color.RGBA{255, 255, 0, 255})
+
+	device.ButtonPress(func(btnIndex int, device *streamdeck.Device, err error) {
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Button %d pressed\n", btnIndex)
+	})
+
+	time.Sleep(5 * time.Second)
+
+	device.ClearButtons()
+	device.Close()
+}
