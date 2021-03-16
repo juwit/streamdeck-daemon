77cacb6dc7fada18ac7f050e1ca6ad6893d8899b
Julien WITTOUCK <julien.wittouck@gmail.com>
2021-03-16 14:05:25 +0100

    ⚗️ : experiment with config loading

---
+++ config/config-service.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Config struct {
+	Brightness int `json:"brightness"`
+	InitialPage string `json:"initial_page"`
+	Pages []Page `json:"pages"`
+}
+
+type Page struct {
+	Name string `json:"name"`
+	Buttons []Button `json:"buttons"`
+}
+
+type Button struct {
+	// the index of the key to put the button on !
+	Key int `json:"key"`
+	// the icon to show on the button
+	Icon string `json:"icon"`
+	// switch on another page when the button pressed
+	SwitchPage string `json:"switch_page"`
+	// text to write when the button is pressed
+	Write string `json:"write"`
+	// command to execute when the button is pressed
+	Command string `json:"command"`
+}
+
+func LoadConfiguration() Config {
+	byteValue, err := ioutil.ReadFile("config.json")
+
+	if err != nil {
+		panic(err)
+	}
+
+	var config Config
+
+	json.Unmarshal(byteValue, &config)
+
+	fmt.Println("Loaded configuration file")
+	fmt.Println(config)
+
+	return config
+}
--- daemon.go
+++ daemon.go
@@ -2,37 +2,63 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-vgo/robotgo"
 	streamdeck "github.com/magicmonkey/go-streamdeck"
 	_ "github.com/magicmonkey/go-streamdeck/devices"
-	"image/color"
 	"os"
 	"os/signal"
 	"syscall"
-	"github.com/go-vgo/robotgo"
 
+	configService "github.com/juwit/streamdeck-daemon/config"
 	"github.com/juwit/streamdeck-daemon/server"
 )
 
+var config configService.Config
+
+var device *streamdeck.Device
+
+var currentPage configService.Page
+
 func main() {
-	device, err := streamdeck.Open()
+	var err error
+	device, err = streamdeck.Open()
 	if err != nil {
 		panic(err)
 	}
 
 	device.ClearButtons()
+	device.ResetComms()
 
 	fmt.Printf("Found device %s\n", device.GetName())
 
+	// load configuration
+	config = configService.LoadConfiguration()
+
+	// set up brightness
+	fmt.Printf("Setting up device brightness to %d\n", config.Brightness)
+	// device.SetBrightness(config.Brightness)
+
+	// switch to initial page
+	fmt.Println("Loading initial page")
+	SwitchToPage(config.InitialPage)
+
 	// A simple yellow button in position 26
-	device.WriteColorToButton(0, color.RGBA{255, 255, 0, 255})
+	// device.WriteColorToButton(0, color.RGBA{255, 255, 0, 255})
 
 	device.ButtonPress(func(btnIndex int, device *streamdeck.Device, err error) {
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("Button %d pressed\n", btnIndex)
+		// fmt.Printf("Button %d pressed\n", btnIndex)
 
-		robotgo.TypeStr("Hello World from streamdeck" + string(btnIndex))
+		// find button on page definition
+		for _, button := range currentPage.Buttons {
+			if button.Key == btnIndex {
+				if button.Write != "" {
+					robotgo.PasteStr(button.Write)
+				}
+			}
+		}
 	})
 
 	setupShutdownHandler(device)
@@ -52,4 +78,25 @@ func setupShutdownHandler(device *streamdeck.Device) {
 
 		os.Exit(0)
 	}()
+}
+
+func SwitchToPage(pageName string) {
+	// first, clearing buttons
+	device.ClearButtons()
+	for _, page := range config.Pages {
+		if page.Name == pageName {
+			// page found !
+			fmt.Println("Page found")
+			currentPage = page
+			for _, button := range page.Buttons {
+				RenderButton(button)
+			}
+		}
+	}
+}
+
+func RenderButton(button configService.Button) {
+	if button.Icon != "" {
+		device.WriteImageToButton(button.Key, button.Icon)
+	}
 }
